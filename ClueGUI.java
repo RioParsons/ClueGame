e147b2ee068e925f62e9085586a6ba886cf04a35
Manali Kale <manaliakale@gmail.com>
2023-04-16 22:17:53 -0600

    Number of players and select character

--- ClueGUI.java
+++ ClueGUI.java
@@ -1,10 +1,13 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class ClueGUI extends JFrame {
     private JPanel window;
@@ -31,6 +34,9 @@ public class ClueGUI extends JFrame {
 
         JButton startButton = new JButton("Start Game");
         startButton.setBounds(400, 450, 150, 50);
+        startButton.addActionListener(e -> {
+            showSetupDialog();
+        });
         add(startButton);
 
         this.add(window);
@@ -62,5 +68,32 @@ public class ClueGUI extends JFrame {
                 }
             }
         });
+    }
+
+    private void showSetupDialog() {
+        // Display a dialog box to get the number of players
+        int numPlayers = Integer.parseInt(JOptionPane.showInputDialog("Enter the number of players (3-6):"));
+
+        // Display a dialog box to select a character for each player
+        String[] characters = {"Miss Scarlet", "Professor Plum", "Mr. Green", "Colonel Mustard", "Mrs. Peacock", "Mrs. Orchid" };
+        String[] options = {"OK", "Cancel"};
+        ImageIcon[] icons = {
+                new ImageIcon(new ImageIcon("resources/scarlet.png").getImage().getScaledInstance(150,350,java.awt.Image.SCALE_SMOOTH )),
+                new ImageIcon(new ImageIcon("resources/plum.png").getImage().getScaledInstance(150,350,java.awt.Image.SCALE_SMOOTH )),
+                new ImageIcon(new ImageIcon("resources/green.png").getImage().getScaledInstance(150,350,java.awt.Image.SCALE_SMOOTH )),
+                new ImageIcon(new ImageIcon("resources/mustard.png").getImage().getScaledInstance(150,350,java.awt.Image.SCALE_SMOOTH )),
+                new ImageIcon(new ImageIcon("resources/peacock.png").getImage().getScaledInstance(150,350,java.awt.Image.SCALE_SMOOTH )),
+                new ImageIcon(new ImageIcon("resources/orchid.png").getImage().getScaledInstance(150,350,java.awt.Image.SCALE_SMOOTH ))
+        };
+        JRadioButton[] buttons = new JRadioButton[6];
+        ButtonGroup group = new ButtonGroup();
+        JPanel panel = new JPanel(new GridLayout(2, 3));
+        for (int i = 0; i < 6; i++) {
+            buttons[i] = new JRadioButton(characters[i], icons[i], i == 0);
+            group.add(buttons[i]);
+            panel.add(buttons[i]);
+        }
+        int result = JOptionPane.showOptionDialog(null, panel, "Select a character",
+                JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
     }
 }
